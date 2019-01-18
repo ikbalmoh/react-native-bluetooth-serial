1541d04ca1faec6f972a72512c85f1cdf18dd419
Ikbal Mohamad Hikmat <hikmat.iqbal@gmail.com>
2019-01-18 11:23:41 +0700

    Update and rename Base64.ts to Base64.js

--- Base64.js
+++ Base64.js
@@ -3,7 +3,7 @@
 
 const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
 const Base64 = {
-  btoa: (input:string = '')  => {
+  btoa: (input = '')  => {
     let str = input;
     let output = '';
 
@@ -23,7 +23,7 @@ const Base64 = {
     return output;
   },
 
-  atob: (input:string = '') => {
+  atob: (input = '') => {
     let str = input.replace(/=+$/, '');
     let output = '';
 
